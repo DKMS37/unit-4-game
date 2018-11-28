ed4a27df6bb32f9619bbb73b73f3e97be2f21584
dkms <john_dk@sbcglobal.net>
2018-11-27 22:00:57 -0800

    making the counter work adding plus 1 each time the crystal images click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,17 @@
-$(".crystal-image").on("click", function() {
+// Here we created a counter, we'll be using this to track the user's total.
+var counter = 0;
 
-    
-    alert("You clicked a crystal!");
+
+$(".crystal-image").on("click", function() {
+    // Each time the user clicks the crystal the counter goes up by 1.
+    counter += 1
+    // We then output the number of times the crystal is clicked.
+    alert("You clicked a crystal!" + counter + " times!");
  
+ 
+ 
+ 
+    $("#counter").html(counter);
   });
+
+  
