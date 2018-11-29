b4641757681a36e2d47ea51e5c7f6ba5570df068
dkms <john_dk@sbcglobal.net>
2018-11-29 15:36:29 -0800

    replacing pictures to more appropriate ones on my theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,63 @@
+$(document).ready(function(){
 
-// Here we establish the "targetNumber" (set to 53 in this example).
-var targetNumber = 53;
-
+var targetNumber = "";
 var wins = 0;
 var losses = 0;
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
 
-$("#targetnumber").text(targetNumber);
-// Here we created a counter, we'll be using this to track the user's total.
-var counter = 0;
-var numberOptions = [10, 5, 3, 7];
 
-for (var i = 0; i < numberOptions.length; i++){
-     var imageCrystal = $("<img>");
-     imageCrystal.addClass(".crystal-image");
+function initial(){
+    targetNumber = Math.floor(Math.random()*101)+19;
+    counter = 0;
+    var imageCrystal = [$(".crystal1"), $(".crystal2"), $(".crytal3"), $(".crytal4")];
+    var clickOptions = [crystal1, crystal2, crystal3, crystal4];
+
+for (var i = 0; i <imageCrystal.length; i++){
+    clickOptions[i]=imageCrystal[i].attr("data-crystalvalue", Math.round(Math.random()*12)+2);
      $("#crystals").append(imageCrystal);
-     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-     
-     
-     console.log(imageCrystal);
-
-
-
-}
-
-
-$(".crystal-image").on("click", function() {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
-    // alert("New score: " + counter);
- 
-
-    // Here we created some logic to "check" if the click counter matches the targetNumber.
-    // Remember, this click event will be triggered with each click.
-    // With each click the counter will increase by 10 and be re-evaluated against target.
-    if (counter === targetNumber) {
-        wins++
-      // If the numbers match we'll celebrate the user's win.
-    //   alert("You win!");
-    }
-    // Here we added an "else if" condition. If the user's counter ever exceeds the targetNumber...
-    else if (counter >= targetNumber) {
-             losses++
-        // Then they are alerted with a loss.
-        // alert("You lose!!");
-      }
-
+     $("#targetnumber").text(targetNumber);
      $("#counter").text(counter);
      $("#wins").text(wins);
      $("#losses").text(losses);
-    });
+     
+};
+};
+
+initial();
+
+
+$(".crystal-image").on("click", function(){
+    counter += parseInt($(this).attr("data-crystalvalue"));
+    $("#counter").text(counter);
+    
+    if (counter === targetNumber) {
+        wins++;
+        initial();
+          
+}
+    else if (counter > targetNumber) {
+             losses++;
+             initial();
+}
+     
+});
+$(".crystal-1").on("click", function(){
+    $(".crystal-1").animate({opacity: "0.4"});
+    $(".crystal-1").animate({opacity: "1"});   
+});
+$(".crystal-2").on("click", function(){
+    $(".crystal-2").animate({opacity: "0.7"});
+    $(".crystal-2").animate({opacity: "1"});
+});
+$(".crystal-3").on("click", function(){
+    $(".crystal-3").animate({opacity: "0.3"});
+    $(".crystal-3").animate({opacity: "1"});
+});
+$(".crystal-4").on("click", function(){
+    $(".crystal-4").animate({opacity: "0.0"});
+    $(".crystal-4").animate({opacity: "1"});
+});
+});
